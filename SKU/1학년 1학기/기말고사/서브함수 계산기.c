bd8cac6ca94d1db97d0c529608b4c7741d4a5356
1ska2suwkd <mickey050110@naver.com>
2024-10-06 18:04:12 +0900

    Create 서브함수 계산기.c

---
+++ SKU/1학년 1학기/기말고사/서브함수 계산기.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//int puls(int a, int b) {
+//	return a + b;
+//}
+//int min(int a, int b) {
+//	return a - b;
+//}
+//int mul(int a, int b) {
+//	return a * b;
+//}
+//double div(double a, double b) {
+//	if (b == 0) {
+//		printf("0으로는 나눌 수 없습니다.")
+//		return 0;
+//	}
+//	return a / b;	
+//}
+//int main() {
+//	int a, b;
+//	int result;
+//	double div1;
+//	scanf("%d %d", &a, &b);
+//	result = puls(a, b);
+//	printf("%d + %d = %d\n",a, b, result);
+//	result = min(a, b);
+//	printf("%d - %d = %d\n", a, b, result);
+//	result = mul(a, b);
+//	printf("%d * %d = %d\n", a, b, result);
+//	div1 = div(a, b);
+//	printf("%d / %d = %lf\n", a, b, div1);
+//
+//	return 0;
+//
+//} 
