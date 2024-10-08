bf90869b762ae29f081ab59b1b43d6f2682d94de
1ska2suwkd <mickey050110@naver.com>
2024-10-08 15:35:57 +0900

    Create 포인터와 배열(1).c

---
+++ SKU/1학년 2학기/중간고사/포인터와 배열(1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a[] = { 10,20,30,40,50 };
+
+	printf("&a[0] = %u\n", &a[0]);
+	printf("&a[1] = %u\n", &a[1]);
+	printf("&a[2] = %u\n", &a[2]);
+
+	printf("a = %u\n", a); // 이 부분이 교수님이 강조하시는 배열의 이름 == 주소값
+
+	return 0;
+}
