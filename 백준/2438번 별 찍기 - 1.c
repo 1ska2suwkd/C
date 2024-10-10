e1dbe47117d84b92b92cb23605900fc7b2d01dce
1ska2suwkd <mickey050110@naver.com>
2024-10-10 18:37:09 +0900

    Update 2438번 별 찍기 - 1.c

--- 백준/2438번 별 찍기 - 1.c
+++ 백준/2438번 별 찍기 - 1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,i=1;
 
+    scanf("%d", &a);
+
+    for (i; i <= a; i++)
+    {
+        for (int j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+}
