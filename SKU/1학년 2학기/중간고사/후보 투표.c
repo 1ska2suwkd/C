a463f59c836b9a20a0b1947c85c5cb459a695b3e
1ska2suwkd <mickey050110@naver.com>
2024-10-19 20:22:45 +0900

    Update 후보 투표.c

--- SKU/1학년 2학기/중간고사/후보 투표.c
+++ SKU/1학년 2학기/중간고사/후보 투표.c
@@ -1,19 +1,19 @@
-//#include <stdio.h>
-//#define SIZE 10
-//int main() {
-//	int ans1;
-//	int freq[SIZE] = { 0 };
-//	while (1) {
-//		printf("몇번 후보자들 선택하시겠습니까?(종료 -1): ");
-//		scanf("%d",&ans1);
-//		
-//		freq[ans1 - 1]++;
-//		if (ans1 == -1)
-//			break;
-//	}
-//	printf("값 득표결과\n");
-//	for (int i = 0; i < SIZE; i++) {
-//		printf("%d\t%d\n", i + 1, freq[i]);
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+#define SIZE 10
+
+int main() {
+	int freq[10] = {0};
+	int ans=0,i;
+
+	while (ans != -1) {
+		printf("몇번 후보자를 선택하시겠습니까?(종료 -1): ");
+		scanf("%d", &ans);
+		freq[ans - 1]++;
+	}
+
+	printf("\n값\t득표결과\n");
+
+	for (i = 0; i < SIZE; i++) {
+		printf("%d\t%d\n", i + 1, freq[i]);
+	}
+}
