148cbe961e65be750f9fe2640f64532bb63daf9d
1ska2suwkd <mickey050110@naver.com>
2024-10-19 21:15:27 +0900

    Update 입력 수 역수.c

--- SKU/1학년 2학기/중간고사/입력 수 역수.c
+++ SKU/1학년 2학기/중간고사/입력 수 역수.c
@@ -1,13 +1,15 @@
-//#include <stdio.h>
-//#define SIZE 5
-//int main() {
-//	int a[SIZE];
-//	for (int i = 0; i < SIZE; i++) {
-//		printf("Á¤¼ö¸¦ ÀÔ·ÂÇÏ½Ã¿À:");
-//		scanf("%d", &a[i]);
-//	}
-//	for (int i = 4; i >= 0; i--) {
-//		printf("%d\n",a[i]);
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+#define SIZE 5
+int main() {
+	int a[SIZE] = { 0 };
+	int i;
+
+	for (i = 0; i < SIZE; i++) {
+		printf("정수를 입력하시오:");
+		scanf("%d", &a[i]);
+	}
+
+	for (i = SIZE-1; i >= 0; i--) {
+		printf("%d\n", a[i]);
+	}
+}
