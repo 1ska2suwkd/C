44d51c58d1fbb8f0f3b3d7168f8b337d8b8704d3
1ska2suwkd <mickey050110@naver.com>
2024-10-19 18:48:28 +0900

    Update 좌석예약.c

--- SKU/1학년 2학기/중간고사/좌석예약.c
+++ SKU/1학년 2학기/중간고사/좌석예약.c
@@ -1,34 +1,42 @@
-//#include <stdio.h>
-//#define SIZE 10
-//int main() {
-// 
-//	char ans1;
-//	int ans2;
-//	int seats[SIZE] = {0};
-//	while (1) {
-//		printf("좌석을 예약하시겠습니까?(y 또는 n)");
-//		scanf(" %c", &ans1);
-//		if (ans1 == 'n')
-//			break;
-//		if (ans1 == 'y') {
-//			printf("-------------------------------\n");
-//			printf("1 2 3 4 5 6 7 8 9 10\n");
-//			printf("-------------------------------\n");
-//			for (int i = 0; i < SIZE; i++) {
-//				printf("%d", seats[i]);
-//			}
-//			printf("\n몇번째 좌석을 예약하시겠습니까?");
-//			scanf("%d", &ans2);
-//			for (int i = 1; i <= SIZE; i++) {
-//				if (ans2 == i && seats[i-1] == 0) {
-//					seats[i-1] = 1;
-//					printf("예약되었습니다.\n");
-//				}
-//				else if (ans2 == i && seats[i-1] == 1) {
-//					printf("이미 예약된 자리입니다. 다른 좌석을 선택하세요.\n");
-//				}
-//			}
-//		}
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+#define SIZE 10
+
+int main() {
+	int a[SIZE] = { 0 };
+	char ans1;
+	int i,ans2;
+	
+	while (1) {
+		printf("좌석을 예약하시겠습니까?(y 또는 n) ");
+		scanf(" %c", &ans1);
+
+		if (ans1 == 'n') return 0;
+
+		printf("-------------------------------\n");
+		printf("1 2 3 4 5 6 7 8 9 10\n");
+		printf("-------------------------------\n");
+		
+		for (i = 0; i < SIZE; i++) {
+			printf("%d ", a[i]);
+		}
+
+		printf("\n몇번째 좌석을 예약하시겠습니까?");
+		scanf("%d", &ans2);
+
+		for (i = 0; i < SIZE; i++) {
+			if (ans2 == i + 1) {
+				if (a[i] == 1) {
+					printf("이미 예약된 자리입니다. 다른 좌석을 선택하세요\n");
+					break;
+				}
+				
+				else {
+					printf("예약되었습니다.\n");
+					a[i] = 1;
+				}
+
+			}
+		}
+	}
+	return 0;
+}
