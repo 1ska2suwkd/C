4fd156ba5fbf46b77a3d825c71de184b2ae4dc2f
1ska2suwkd <mickey050110@naver.com>
2024-10-19 22:59:28 +0900

    Update 배열과함수 예제1.c

--- SKU/1학년 2학기/중간고사/배열과함수 예제1.c
+++ SKU/1학년 2학기/중간고사/배열과함수 예제1.c
@@ -1,22 +1,23 @@
-//#include <stdio.h>
-//#define students 5
-//int get_average(int score[], int n);
-//
-//int main() {
-//	int avg;
-//	int grade[students] = { 1 ,2 ,3 ,4 ,5 };
-//
-//	avg = get_average(grade, students);
-//	printf("Æò±ÕÀº %dÀÔ´Ï´Ù.", avg);
-//	return 0;
-//}
-//
-//int get_average(int score[], int n) {
-//	int sum = 0;
-//	int i;
-//
-//	for (i = 0; i < n; i++) {
-//		sum += score[i];
-//	}
-//	return sum / n;
-//}
+#include <stdio.h>
+#define STUDENTS 5
+int get_average(int score[], int n);
+
+int main() {
+	int grade[STUDENTS] = { 1,2,3,4,5 };
+	int avg;
+
+	avg = get_average(grade, STUDENTS);
+	printf("평균은 %d입니다.\n", avg);
+
+	return 0;
+}
+
+int get_average(int score[], int n) {
+	int sum = 0;
+	int i;
+	for (i = 0; i < STUDENTS; i++) {
+		sum += score[i];
+	}
+
+	return sum / STUDENTS;
+}
