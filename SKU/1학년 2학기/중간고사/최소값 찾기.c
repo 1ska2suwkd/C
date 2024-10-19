ee4128b4c14d755d56009e0f69d41675c5608344
1ska2suwkd <mickey050110@naver.com>
2024-10-19 19:53:21 +0900

    Update 최소값 찾기.c

--- SKU/1학년 2학기/중간고사/최소값 찾기.c
+++ SKU/1학년 2학기/중간고사/최소값 찾기.c
@@ -1,27 +1,31 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//#define SIZE 10
-//int main() {
-//	int prices[SIZE] = { 0 };
-//	int minimum;
-//
-//	printf("---------------------------------------\n");
-//	printf("1 2 3 4 5 6 7 8 9 10\n");
-//	printf("---------------------------------------\n");
-//	
-//	srand((unsigned)time(NULL));
-//	for (int i = 0; i < SIZE; i++) {
-//		prices[i] = (rand() % 100) + 1;
-//		printf("%-3d", prices[i]);
-//	}
-//	printf("\n\n");
-//	minimum = prices[0];
-//	for (int i = 1; i < SIZE; i++)
-//	{
-//		if (prices[i] < minimum)
-//			minimum = prices[i];
-//	}
-//	printf("ÃÖ¼Ò°ªÀº %dÀÔ´Ï´Ù.\n", minimum);
-//	return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 10
+
+int main() {
+	int price[SIZE] = {0};
+	int i,minimum;
+
+	printf("---------------------------------------\n");
+	printf("1 2 3 4 5 6 7 8 9 10\n");
+	printf("---------------------------------------\n");
+
+	srand((unsigned)time(NULL)); //srand 함수는 난수 생성기의 seed를 설정,
+								                //time(NULL)은 현재시간을 반환해줌
+
+	for (i = 0; i < SIZE; i++) {
+		price[i] = (rand() % 100) + 1; //srand로 설정한 난수들을 설정에 맞게 rand로 반환
+		printf("%-3d ", price[i]);
+	}
+
+	minimum = price[0];
+
+	for (i = 0; i < SIZE; i++) {
+		if (minimum > price[i]) minimum = price[i];
+	}
+
+	printf("\n\n최소값은 %d입니다.\n", minimum);
+
+	return 0;
+}
