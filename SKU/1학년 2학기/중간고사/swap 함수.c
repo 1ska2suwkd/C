0586048e38ff8609925d09614b5503d8522c2df4
1ska2suwkd <mickey050110@naver.com>
2024-10-28 18:30:25 +0900

    Update swap 함수.c

--- SKU/1학년 2학기/중간고사/swap 함수.c
+++ SKU/1학년 2학기/중간고사/swap 함수.c
@@ -6,14 +6,14 @@ int main() {
 	int a = 100;
 	int b = 200;
 
-	swap(&a, &b);
+	swap(&a, &b); //swap 함수에 변수 a와 b의 주소를 넘겨줌
 
-	printf("%d %d", a, b);
+	printf("%d %d", a, b); 
 
 	return 0;
 }
 
-void swap(int* px, int* py) {
+void swap(int* px, int* py) { //a와 b의 주소를 각각 포인터 px와 py랑 연결
 	int tmp;
 	tmp = *px;
 	*px = *py;
