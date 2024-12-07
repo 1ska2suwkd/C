d597c3f6523e2951d7027ce0b82b04e6505586f1
1ska2suwkd <mickey050110@naver.com>
2024-12-07 21:18:52 +0900

    Update 영어 문장 외우기(중간고사).c
    함수를 이용하여 반복되는 문장 제거

--- Program/영어 문장 외우기(중간고사).c
+++ Program/영어 문장 외우기(중간고사).c
@@ -3,6 +3,7 @@
 #define QUESTION 21
 
 int comparision(char ans[], char a[]);
+void ps(char ans[][100], int wrong[], char problem[][100]);
 
 int main() {
     int i, j, choice = -1;
@@ -11,21 +12,21 @@ int main() {
     int wrong_count;     // 틀린 문제 개수
 
     char switzerland[15][100] = {
-        {"Switzerland is in Central Europe"},      
-        {"The capital city is Bern"},                                           //2.수도
-        {"The currency is the CHF"},                                            //3.화폐
-        {"The national flower is the edelweiss"},                               //4.꽃
-        {"The national instrument is the alphorn"},                             //5.악기
-        {"The national music is yodel"},                                        //6.음악
-        {"The national anthem is Swiss Psalm"},                                 //7.국가
-        {"Traditional food is fondue"},                                         //8.음식
-        {"Traditional clothing is the tracht"},                                 //9.옷
-        {"The population is about 8 million"},                                  //10.인구
-        {"The official language is Italian"},                                   //11.언어
-        {"Famous people are Leonhard Euler and Albert Einstein"},               //12.유명인
-        {"The colors on the flag are red and white"},                           //13.국기 색
-        {"The president is Viola Amherd"},                                      //14.대통령
-        {"A popular tourist attraction is the Matterhorn"},                     //15. 관광지
+    {"Switzerland is in Central Europe"},
+    {"The capital city is Bern"},                                           //2.수도
+    {"The currency is the CHF"},                                            //3.화폐
+    {"The national flower is the edelweiss"},                               //4.꽃
+    {"The national instrument is the alphorn"},                             //5.악기
+    {"The national music is yodel"},                                        //6.음악
+    {"The national anthem is Swiss Psalm"},                                 //7.국가
+    {"Traditional food is fondue"},                                         //8.음식
+    {"Traditional clothing is the tracht"},                                 //9.옷
+    {"The population is about 8 million"},                                  //10.인구
+    {"The official language is Italian"},                                   //11.언어
+    {"Famous people are Leonhard Euler and Albert Einstein"},               //12.유명인
+    {"The colors on the flag are red and white"},                           //13.국기 색
+    {"The president is Viola Amherd"},                                      //14.대통령
+    {"A popular tourist attraction is the Matterhorn"},                     //15. 관광지
     };
     char idioms[5][100] = {
         {"I grew up in Ansan"},
@@ -48,107 +49,13 @@ int main() {
         scanf("%d", &choice);
 
         if (choice == 1) {
-            wrong_count = 0; // 틀린 문제 개수 초기화
-            for (i = 0; i < 15; i++) {
-                printf("%d: ", i + 1);
-                scanf(" %[^\n]", ans[i]);
-
-                if (comparision(ans[i], switzerland[i]) == -1) {
-                    printf("오답\n");
-                    wrong[wrong_count++] = i; // 틀린 문제 번호 저장
-                }
-                else {
-                    printf("정답\n");
-                }
-            }
-
-            // 틀린 문제 다시 풀기
-            while (wrong_count > 0) {
-                printf("\n틀린 문제를 다시 풀어봅시다.\n");
-                int new_wrong_count = 0; // 새로운 틀린 문제 개수
-                for (j = 0; j < wrong_count; j++) {
-                    i = wrong[j]; // 틀린 문제 인덱스
-                    printf("%d: ", i + 1);
-                    scanf(" %[^\n]", ans[i]);
-
-                    if (comparision(ans[i], switzerland[i]) == -1) {
-                        printf("오답\n");
-                        wrong[new_wrong_count++] = i; // 아직 틀린 문제 저장
-                    }
-                    else {
-                        printf("정답\n");
-                    }
-                }
-                wrong_count = new_wrong_count; // 틀린 문제 개수 업데이트
-            }
+            ps(ans,wrong,switzerland);
         }
         else if (choice == 2) {
-            wrong_count = 0;
-            for (i = 0; i < 5; i++) {
-                printf("%d: ", i + 1);
-                scanf(" %[^\n]", ans[i]);
-
-                if (comparision(ans[i], idioms[i]) == -1) {
-                    printf("오답\n");
-                    wrong[wrong_count++] = i;
-                }
-                else {
-                    printf("정답\n");
-                }
-            }
-
-            while (wrong_count > 0) {
-                printf("\n틀린 문제를 다시 풀어봅시다.\n");
-                int new_wrong_count = 0;
-                for (j = 0; j < wrong_count; j++) {
-                    i = wrong[j];
-                    printf("%d: ", i + 1);
-                    scanf(" %[^\n]", ans[i]);
-
-                    if (comparision(ans[i], idioms[i]) == -1) {
-                        printf("오답\n");
-                        wrong[new_wrong_count++] = i;
-                    }
-                    else {
-                        printf("정답\n");
-                    }
-                }
-                wrong_count = new_wrong_count;
-            }
+            ps(ans, wrong,idioms);
         }
         else if (choice == 3) {
-            wrong_count = 0;
-            for (i = 0; i < 6; i++) {
-                printf("%d: ", i + 1);
-                scanf(" %[^\n]", ans[i]);
-
-                if (comparision(ans[i], myself[i]) == -1) {
-                    printf("오답\n");
-                    wrong[wrong_count++] = i;
-                }
-                else {
-                    printf("정답\n");
-                }
-            }
-
-            while (wrong_count > 0) {
-                printf("\n틀린 문제를 다시 풀어봅시다.\n");
-                int new_wrong_count = 0;
-                for (j = 0; j < wrong_count; j++) {
-                    i = wrong[j];
-                    printf("%d: ", i + 1);
-                    scanf(" %[^\n]", ans[i]);
-
-                    if (comparision(ans[i], myself[i]) == -1) {
-                        printf("오답\n");
-                        wrong[new_wrong_count++] = i;
-                    }
-                    else {
-                        printf("정답\n");
-                    }
-                }
-                wrong_count = new_wrong_count;
-            }
+            ps(ans, wrong,myself);
         }
         else break;
     }
@@ -162,3 +69,42 @@ int comparision(char ans[], char a[]) {
         return -1; // 오답
 }
 
+void ps(char ans[][100],int wrong[],char problem[][100]) {
+    int i, j, choice = -1;
+    int wrong_count = 0; // 틀린 문제 개수 초기화
+
+
+
+    for (i = 0; i < 15; i++) {
+        printf("%d: ", i + 1);
+        scanf(" %[^\n]", ans[i]);
+
+        if (comparision(ans[i], problem[i]) == -1) {
+            printf("오답\n");
+            wrong[wrong_count++] = i; // 틀린 문제 번호 저장
+        }
+        else {
+            printf("정답\n");
+        }
+    }
+
+    // 틀린 문제 다시 풀기
+    while (wrong_count > 0) {
+        printf("\n틀린 문제를 다시 풀어봅시다.\n");
+        int new_wrong_count = 0; // 새로운 틀린 문제 개수
+        for (j = 0; j < wrong_count; j++) {
+            i = wrong[j]; // 틀린 문제 인덱스
+            printf("%d: ", i + 1);
+            scanf(" %[^\n]", ans[i]);
+
+            if (comparision(ans[i], problem[i]) == -1) {
+                printf("오답\n");
+                wrong[new_wrong_count++] = i; // 아직 틀린 문제 저장
+            }
+            else {
+                printf("정답\n");
+            }
+        }
+        wrong_count = new_wrong_count; // 틀린 문제 개수 업데이트
+    }
+}
