d69c400330cc8ccd36e98aa222760bb68adabc12
1ska2suwkd <mickey050110@naver.com>
2024-12-10 17:10:49 +0900

    Update 구조체 예제 (1).c

--- SKU/1학년 2학기/기말고사/구조체 예제 (1).c
+++ SKU/1학년 2학기/기말고사/구조체 예제 (1).c
@@ -10,7 +10,7 @@ int main() {
 	struct student s;
 
 	s.number = 20240000;
-	strcpy(s.name, "이채헌");
+	strcpy(s.name, "이채헌"); // s.name = "이채헌" 을 할 경우 배열의 이름은 배열의 주소이므로 문자열에 주소를 참조하게 됨.
 	s.grade = 4.5;
 
 	printf("학번: %d\n", s.number);
