d8a58025f14469793bee416173da4850aef83d9c
1ska2suwkd <mickey050110@naver.com>
2024-12-14 21:52:19 +0900

    Update 파일 열기.c

--- SKU/1학년 2학기/기말고사/파일 열기.c
+++ SKU/1학년 2학기/기말고사/파일 열기.c
@@ -3,7 +3,7 @@
 int main() {
 	FILE* fp = NULL; // fp를 포인터로 선언하는 것은 FILE 구조체의 크기가 커서 (다른 이유도 많음)
 
-	fp = fopen("sameple.txt", "w"); // FILE *fp는 파일을 열 때 fopen 함수에 의해 생성된 FILE 구조체의 주소를 저장한다.
+	fp = fopen("sample.txt", "w"); // FILE *fp는 파일을 열 때 fopen 함수에 의해 생성된 FILE 구조체의 주소를 저장한다.
 
 	if (fp == NULL)
 		printf("파일 열기 실패\n");
