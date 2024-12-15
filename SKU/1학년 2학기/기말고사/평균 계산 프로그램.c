ba5f516c51d515c03bc2b58bc36e447e70a4cd79
1ska2suwkd <mickey050110@naver.com>
2024-12-15 19:15:23 +0900

    Update 평균 계산 프로그램.c

--- SKU/1학년 2학기/기말고사/평균 계산 프로그램.c
+++ SKU/1학년 2학기/기말고사/평균 계산 프로그램.c
@@ -38,8 +38,8 @@ int main() {
 		exit(1);
 	}
 
-	while (!feof(fp)) {
-		if(fscanf(fp, "%d %s %f", &s.number, s.name, &s.score) == 3){
+	while (!feof(fp)) { // feof 함수는 파일이 eof를 만나고 나서도 한 번 참이기 때문에 반복문이 한 번 더 실행 됨
+		if(fscanf(fp, "%d %s %f", &s.number, s.name, &s.score) == 3){ // 이 조건문이 없으면 count 변수가 한번 더 증가되어서 평균 값이 이상하게 나옴
 			total += s.score;
 			count++;
 		}
